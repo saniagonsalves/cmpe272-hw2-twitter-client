b0e3ea36fa7f2c38b5a1a6a72432debea0282495
shreyaghotankar <shreya.ghotankar@sjsu.edu>
2020-09-25 12:11:41 -0700

    Added unit tests..

--- src/main/java/org/example/DeleteTweet.java
+++ src/main/java/org/example/DeleteTweet.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import twitter4j.Status;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
@@ -14,23 +15,39 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DeleteTweet extends HttpServlet {
 
+	private Twitter twitter;
+	public DeleteTweet() {
+		twitter = TwitterFactory.getSingleton();
+	}
+	
+	public DeleteTweet(Twitter twitter) {
+		this.twitter = twitter;
+	}
+
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String tweetIdStr = req.getParameter("tweetId");
     long tweetId = Long.parseLong(tweetIdStr);
     Status result = null;
+    String errorMessage = null;
 
     try {
-      result = TwitterFactory.getSingleton().destroyStatus(tweetId);
-      System.out.println(result.toString());
+      result = twitter.destroyStatus(tweetId);
+      //System.out.println(result.toString());
     } catch (TwitterException e) {
       e.printStackTrace();
+      errorMessage = e.getErrorMessage();
     }
 
     if (result != null) {
       RequestDispatcher requestDispatcher = req.getRequestDispatcher("deleted.jsp");
       resp.setHeader("tid", Long.toString(result.getId()));
       requestDispatcher.include(req, resp);
+    }
+    else {
+    	RequestDispatcher requestDispatcher = req.getRequestDispatcher("error.jsp");
+        resp.setHeader("error", errorMessage);
+        requestDispatcher.include(req, resp);
     }
   }
 }
